565203ac670de7c18c2c86d0f9cc49c5321845ff
Lukas Lanzner <LanznerLukas@dotspace.dev>
2024-06-11 11:44:33 +0200

    Tree pass until "find by matr (existing)"

--- dhbwstudenttree.c
+++ dhbwstudenttree.c
@@ -1,9 +1,9 @@
 /*
  ============================================================================
  Aufgabe     : Datenstrukturen - Woche 5
- Autor       :
+ Autor       : Lukas Lanzner
  Matrikel    :
- Version     :
+ Version     : 0.1
  ============================================================================
  */
 #include <stdbool.h>
@@ -14,46 +14,46 @@
 //DIESE METHODEN NICHT AENDERN
 
 StudentTP StudentTPAlloc(Student_p newStudent) {
-	StudentTP newStudentTP = malloc(sizeof(StudentT));
+    StudentTP newStudentTP = malloc(sizeof(StudentT));
 
-	newStudentTP->student = newStudent;
-	newStudentTP->lchild = NULL;
-	newStudentTP->rchild = NULL;
+    newStudentTP->student = newStudent;
+    newStudentTP->lchild = NULL;
+    newStudentTP->rchild = NULL;
 
-	return newStudentTP;
+    return newStudentTP;
 }
 
 
 void StudentTPFree(StudentTP tree) {
-	if (tree == NULL)
-		return;
+    if (tree == NULL)
+        return;
 
-	StudentFree(tree->student);
-	free(tree);
-	return;
+    StudentFree(tree->student);
+    free(tree);
+    return;
 }
 
-void StudentTFree(StudentTP* root_adr) {
+void StudentTFree(StudentTP *root_adr) {
 
-	StudentTP current = *root_adr;
+    StudentTP current = *root_adr;
 
-	if (current == NULL) {
-		return;
-	}
-	StudentTFree(&(current->lchild));
-	StudentTFree(&(current->rchild));
+    if (current == NULL) {
+        return;
+    }
+    StudentTFree(&(current->lchild));
+    StudentTFree(&(current->rchild));
 
-	StudentTPFree(current);
-	return;
+    StudentTPFree(current);
+    return;
 }
 
 StudentTP deepTPCopy(StudentTP info) {
-	if (info == NULL)
-		return NULL;
-	StudentTP copy = StudentTPAlloc(deepCopy(info->student));
-	copy->lchild = NULL;
-	copy->rchild = NULL;
-	return copy;
+    if (info == NULL)
+        return NULL;
+    StudentTP copy = StudentTPAlloc(deepCopy(info->student));
+    copy->lchild = NULL;
+    copy->rchild = NULL;
+    return copy;
 }
 
 
@@ -67,36 +67,94 @@ StudentTP deepTPCopy(StudentTP info) {
 
 
 bool StudentTImplemented() {
-	//TODO: hier auf true aendern, damit Ihre Implementierung getestet wird
-	return false;
+    return true;
 }
 
-bool StudentTContainsStudent(StudentTP* root_adr, Student_p student) {
-	return false;
+bool StudentTContainsStudent(StudentTP *root_adr, Student_p student) {
+    StudentTP node = *root_adr;
+
+    if (node == NULL)return false;
+
+    if (node->student == NULL)
+        return false;
+
+    if (node->student->matrnr == student->matrnr)
+        return true;
+
+    bool res_left = StudentTContainsStudent(&node->lchild, student);
+    if (res_left) return true;
+
+    return StudentTContainsStudent(&node->rchild, student);
 }
 
-StudentTP StudentTFindByMatr(StudentTP* root_adr, int matrnr) {
-	return NULL;
+StudentTP StudentTFindByMatr(StudentTP *root_adr, int matrnr) {
+    StudentTP node = *root_adr;
+
+    if (node)return NULL;
+    if (node->student == NULL)
+        return NULL;
+
+    if (node->student->matrnr == matrnr)
+        return node;
+
+    StudentTP res_left = StudentTFindByMatr(&node->lchild, matrnr);
+    if (res_left != NULL) return res_left;
+
+    return StudentTFindByMatr(&node->rchild, matrnr);
 }
 
-StudentTP StudentTFindByName(StudentTP* root_adr, char *lastname) {
-	return NULL;
+StudentTP StudentTFindByName(StudentTP *root_adr, char *lastname) {
+    return NULL;
 }
 
-StudentLP* StudentTToSortedList(StudentTP* root_adr) {
-	return NULL;
+StudentLP *StudentTToSortedList(StudentTP *root_adr) {
+    return NULL;
 }
 
-void StudentTInsertSorted(StudentTP* root_adr, Student_p newStudent) {
+void StudentTInsertSorted(StudentTP *root_adr, Student_p newStudent) {
+    StudentTP node = *root_adr;
 
+    if (node == NULL) {
+        (*root_adr) = StudentTPAlloc(newStudent);
+        return;
+    }
+
+    // check for collisions
+    if (newStudent->matrnr == node->student->matrnr)
+        return;
+
+    // figure out whether to insert at right or left node
+    if (newStudent->matrnr < node->student->matrnr) {
+        StudentTInsertSorted(&node->lchild, newStudent);
+    } else {
+        StudentTInsertSorted(&node->rchild, newStudent);
+    }
 }
 
-int StudentTSize(StudentTP* root_adr) {
-	return -1;
+int StudentTSize(StudentTP *root_adr) {
+    if (*root_adr == NULL) {
+        return 0;
+    }
+
+    StudentTP node = *root_adr;
+
+    int left_size = StudentTSize(&node->lchild);
+    int right_size = StudentTSize(&node->rchild);
+
+    return 1 + left_size + right_size;
 }
 
-int StudentTDepth(StudentTP* root_adr) {
-	return -1;
+int StudentTDepth(StudentTP *root_adr) {
+    if (*root_adr == NULL) {
+        return 0;
+    }
+
+    StudentTP node = *root_adr;
+
+    int left_depth = StudentTDepth(&node->lchild);
+    int right_depth = StudentTDepth(&node->rchild);
+
+    return 1 + (left_depth > right_depth ? left_depth : right_depth);
 }
 
 
