a888276b7ca929704db907728f7f046893ea18ae
Lukas Lanzner <LanznerLukas@dotspace.dev>
2024-06-25 10:30:07 +0200

    algo lab 3 - merge

--- dhbwsortmerge.c
+++ dhbwsortmerge.c
@@ -17,10 +17,52 @@
 
 //Auf true setzen, damit Array-MergeSort getestet wird
 bool MergeSortArrayImplemented() {
-	return false;
+	return true;
 }
 //MergeSort mit Array
 void MergeSortArray(Student_p *array, int count) {
+    Student_p *temp = (Student_p *)malloc(count * sizeof(Student_p));
+    if (temp == NULL) {
+        fprintf(stderr, "Memory allocation error\n");
+        exit(1);
+    }
+
+    mergeSort(array, temp, 0, count - 1);
+
+    free(temp);
+}
+
+void mergeSort(Student_p *array, Student_p *temp, int left, int right) {
+    if (left < right) {
+        int mid = (left + right) / 2;
+        mergeSort(array, temp, left, mid);
+        mergeSort(array, temp, mid + 1, right);
+        merge(array, temp, left, mid, right);
+    }
+}
+
+void merge(Student_p *array, Student_p *temp, int left, int mid, int right) {
+    int i = left, j = mid + 1, k = left;
+
+    while (i <= mid && j <= right) {
+        if (array[i]->matrnr <= array[j]->matrnr) {
+            temp[k++] = array[i++];
+        } else {
+            temp[k++] = array[j++];
+        }
+    }
+
+    while (i <= mid) {
+        temp[k++] = array[i++];
+    }
+
+    while (j <= right) {
+        temp[k++] = array[j++];
+    }
+
+    for (i = left; i <= right; i++) {
+        array[i] = temp[i];
+    }
 }
 
 
@@ -28,8 +70,55 @@ void MergeSortArray(Student_p *array, int count) {
 
 //Auf true setzen, damit Listen-MergeSort getestet wird
 bool MergeSortListImplemented() {
-	return false;
+	return true;
 }
 //MergeSort mit Liste
 void MergeSortList(StudentLP *anchor_adr) {
+    StudentLP head = *anchor_adr;
+    StudentLP a, b;
+
+    if ((head == NULL) || (head->next == NULL)) {
+        return;
+    }
+
+    splitList(head, &a, &b);
+
+    MergeSortList(&a);
+    MergeSortList(&b);
+
+    *anchor_adr = mergeLists(a, b);
 }
+
+StudentLP mergeLists(StudentLP l1, StudentLP l2) {
+    if (!l1) return l2;
+    if (!l2) return l1;
+
+    if (l1->student->matrnr < l2->student->matrnr) {
+        l1->next = mergeLists(l1->next, l2);
+        return l1;
+    } else {
+        l2->next = mergeLists(l1, l2->next);
+        return l2;
+    }
+}
+
+void splitList(StudentLP source, StudentLP *front, StudentLP *back) {
+    StudentLP slow = source;
+    StudentLP fast = source->next;
+
+    while (fast != NULL) {
+        fast = fast->next;
+        if (fast != NULL) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+    }
+
+    *front = source;
+    *back = slow->next;
+    slow->next = NULL;
+}
+
+
+
+
--- dhbwsortmerge.h
+++ dhbwsortmerge.h
@@ -23,11 +23,15 @@
 bool MergeSortArrayImplemented();
 //MergeSort mit Array
 void MergeSortArray(Student_p *array, int count);
+void mergeSort(Student_p *array, Student_p *temp, int left, int right);
+void merge(Student_p *array, Student_p *temp, int left, int mid, int right);
 
 
 //Auf true setzen, damit Listen-MergeSort getestet wird
 bool MergeSortListImplemented();
 //MergeSort mit Liste
 void MergeSortList(StudentLP *anchor_adr);
+StudentLP mergeLists(StudentLP l1, StudentLP l2);
+void splitList(StudentLP source, StudentLP *front, StudentLP *back);
 
 #endif /* DHBWSORTMERGE_H_ */
